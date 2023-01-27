66128800dadd676526c71e079b1e966fbfc35378
Cristinaxxx <117525933+Cristinaxxx@users.noreply.github.com>
2023-01-27 10:42:10 +0100

    Initial commit

---
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LimitedNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        List<Integer> inputs = new ArrayList<>();
+        
+        while(true) {
+            int input = Integer.valueOf(scanner.nextLine());            
+            if(input < 0) {
+                break;
+            }
+            inputs.add(input);
+        }
+        
+        inputs.stream()
+            .filter(value -> value <= 5 && value >= 1)
+            .forEach(value -> System.out.println(value));                
+
+    }
+}
