638bbe256cb17fcbb7310b1eab84c0cba81247fd
Cristinaxxx <117525933+Cristinaxxx@users.noreply.github.com>
2023-01-27 10:21:34 +0100

    Initial commit

---
+++ part09-Part09_05.TacoBoxes/src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        TripleTacoBox tripleBox = new TripleTacoBox();
+        CustomTacoBox customBox = new CustomTacoBox(10);
+
+        
+    }
+}
