2da6480903e39535d7fb2efeb1d5ca73a0043a39
Cristinaxxx <117525933+Cristinaxxx@users.noreply.github.com>
2023-01-29 11:12:09 +0100

    Initial commit

---
+++ part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        Checker checker = new Checker();
+        
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+        
+        if(checker.isDayOfWeek(input) || checker.allVowels(input) || checker.timeOfDay(input)) {
+            System.out.println("The form is correct.");
+        } else {
+            System.out.println("The form is incorrect.");
+        }
+      
+    }
+}
