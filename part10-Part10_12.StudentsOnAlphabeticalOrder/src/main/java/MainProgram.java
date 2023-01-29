e98673d0bb30c57af75a4020d1d6a1388d65825e
Cristinaxxx <117525933+Cristinaxxx@users.noreply.github.com>
2023-01-29 11:10:16 +0100

    Initial commit

---
+++ part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/MainProgram.java
@@ -0,0 +1,13 @@
+
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        
+        Student first = new Student("Matti");
+        Student second = new Student("Cristina");
+        System.out.println(first.compareTo(second));
+        
+        
+    }
+}
